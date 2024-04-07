0a3347fdcbe4dce114ad4126dd0cf86ae552880a
Buenobarbie <barbarabueno140104@gmail.com>
2024-04-07 20:28:54 -0300

    :sparkles: splines plot

--- Metodologia-Numerica/cubic-splines.py
+++ Metodologia-Numerica/cubic-splines.py
@@ -1,7 +1,26 @@
 import numpy as np
+from gaussian_elimination import gauss_elimination
+import matplotlib.pyplot as plt
 
-x = np.array([1,3,5,8])
-y = np.array([2,3,9,10])
+h = 0.1
+t0 = 0
+n_pontos = 100
+t = np.arange(t0, n_pontos * h, h)
+
+def f3grau(t, a, b, c, d):
+    return a*t**3 + b*t**2 + c*t + d
+
+y = f3grau(t, 1, 2, 3, 4)
+
+
+# Example
+# x = np.array([1,3,5,8])
+# y = np.array([2,3,9,10])
+
+# Solution
+# [[  0.1995614   -0.59868421   0.3004386    2.09868421]
+#  [ -0.37280702   4.55263158 -15.15350877  17.55263158]
+#  [  0.11549708  -2.77192982  21.46929825 -43.48538012]]
 
 
 
@@ -32,7 +51,7 @@ def get_equations(x, y):
         equations[2*i+1][i*4 + 1] = x[i+1]**2
         equations[2*i+1][i*4 + 2] = x[i+1]
         equations[2*i+1][i*4 + 3] = 1
-        equations[2*i+1][4*n] = y[i]
+        equations[2*i+1][4*n] = y[i+1]
 
     for i in range(n-1):
         # P'n(xn+1) = P'n+1(xn+1)
@@ -74,14 +93,99 @@ def get_splines(x,y):
     
     equations = get_equations(x,y)
     variable_matrix = equations[:, :4*n]
-    constant_matrix = equations[:, 4*n]
+    constant_matrix = equations[:, 4*n].reshape(4*n,1)
+
+    # print(variable_matrix)
+    # print(constant_matrix)
+
+    # variable_matrix = np.array([[1,1,3],[0,1,3],[-1,3,0]])
+    # constant_matrix = np.array([[1],[3],[5]])
 
     # Import gaussian elimination and solve
-    
+    solution = gauss_elimination(variable_matrix, constant_matrix)
+
     # Splines
     # a0*x³ + b0*x² + c0*x + d0 -> [a0, b0, c0, d0]
-    splines = np.zeros((n, 4), dtype=float)
+    splines = solution.reshape(n, 4)
+
+    return splines
+
+# print(get_splines(t,y))
+
+# def spline_aproximation(x, splines):
+#     n = len(splines)
+#     y = np.zeros(len(x))
+#     for i in range(n):
+#         a = splines[i][0]
+#         b = splines[i][1]
+#         c = splines[i][2]
+#         d = splines[i][3]
+
+#         for j in range(len(x)):
+#             if x[j] >= t[i] and x[j] <= t[i+1]:
+#                 y[j] = a*x[j]**3 + b*x[j]**2 + c*x[j] + d
+
+#     return y
+
+def spline_aproximation(x, splines, n=10):
+    n_splines = len(splines)
+    t = np.arange(x[0], x[-1], (x[-1] - x[0])/(n*n_splines))
+    y = np.zeros(n_splines*n + 1)
+    for i in range(n_splines):
+        a = splines[i][0]
+        b = splines[i][1]
+        c = splines[i][2]
+        d = splines[i][3]
+
+        for j in range(n):
+            index = i*n + j
+            y[index] = a*t[index]**3 + b*t[index]**2 + c*t[index] + d
+
+    t = np.append(t, x[-1])
+    y[-1] = a*x[-1]**3 + b*x[-1]**2 + c*x[-1] + d
+    return y, t
+
+y_aproximado, t_splines = spline_aproximation(t, get_splines(t,y))
+
+# Visualizar solução analítica e aproximada
+plt.figure(figsize=(8, 6))
+plt.plot(t, y, label='Solução Analítica')
+plt.plot(t_splines, y_aproximado, label='Aproximação por Splines Cúbicas', linestyle='--')
+plt.xlabel('Tempo')
+plt.ylabel('Posição')
+plt.title('Comparação entre Solução Analítica e aproximação por Splines Cúbicas de função de 3° grau')
+plt.legend()
+plt.grid(True)
+plt.show()
 
 
 
 
+
+ # Gerar tabela para latex
+# def imprimir_tabela(tempo, solucao_analitica, aproximacao_spline):
+#   """
+#   Função que recebe 3 arrays de NumPy e imprime uma tabela formatada.
+
+#   Argumentos:
+#     tempo: Array de NumPy com os valores de tempo.
+#     solucao_analitica: Array de NumPy com os valores da solução analítica.
+#     aproximacao_spline: Array de NumPy com os valores da aproximação por splines cúbicas naturais.
+
+#   Retorno:
+#     Nenhum. A função imprime a tabela formatada no console.
+#   """
+
+#   # Imprimindo cabeçalho da tabela
+#   print("Tempo & Solução Analítica & Aproximação por Splines Cúbicas Naturais\\")
+#   print("\\hline")
+
+#   # Percorrendo os arrays e imprimindo cada linha da tabela
+#   for i in range(len(tempo)):
+#     print(f"{tempo[i]:.1f} & {solucao_analitica[i]:.5f} & {aproximacao_spline[i]:.5f}")
+
+#   # Imprimindo linha final da tabela
+#   print("\\hline")
+
+# # Gerar tabela
+# # imprimir_tabela(t, y, y_aproximado)
--- Metodologia-Numerica/gaussian_elimination.py
+++ Metodologia-Numerica/gaussian_elimination.py
@@ -10,7 +10,7 @@ def gauss_elimination(a_matrix,b_matrix):
         print("ERROR: Constant vector incorrectly sized")
         return
      
-    #initialization of nexessary variables
+    #initialization of necessary variables
     n=len(b_matrix)
     m=n-1
     i=0
@@ -20,40 +20,55 @@ def gauss_elimination(a_matrix,b_matrix):
     
     #create our augmented matrix throug Numpys concatenate feature
     augmented_matrix = np.concatenate((a_matrix, b_matrix,), axis=1, dtype=float)
-    print(f"the initial augmented matrix is: {new_line}{augmented_matrix}")
-    print("solving for the upper-triangular matrix:")
+    # print(f"the initial augmented matrix is: {new_line}{augmented_matrix}")
+    # print("solving for the upper-triangular matrix:")
     
     
     #applying gauss elimination:
     while i<n:
-        if augmented_matrix[i][i]==0.0: #fail-safe to eliminate divide by zero erroor!
+        # Partial Pivoting
+        for p in range(i+1,n):
+            if abs(augmented_matrix[i,i]) < abs(augmented_matrix[p,i]):
+                augmented_matrix[[p,i]] = augmented_matrix[[i,p]]
+
+        if augmented_matrix[i,i]==0.0:  #fail-safe to eliminate divide by zero erroor!
             print("Divide by zero error")
             return
         for j in range(i+1,n):
             scaling_factor=augmented_matrix[j][i]/augmented_matrix[i][i]
             augmented_matrix[j]=augmented_matrix[j]-(scaling_factor * augmented_matrix[i])
-            print(augmented_matrix) #not needed, but nice to visualize the process
+            # print(augmented_matrix) #not needed, but nice to visualize the process
             
         i=i+1
+
+    #backwords substitution!
+    # print(augmented_matrix)
+    x[m]=augmented_matrix[m][n]/augmented_matrix[m][m]
+    for k in range(n-2,-1,-1):
+        x[k]=augmented_matrix[k][n]
+        for j in range(k+1,n):
+            x[k]=x[k]-augmented_matrix[k][j] * x[j]
+        x[k]=x[k]/augmented_matrix[k][k]
     
-        #backwords substitution!
-        x[m]=augmented_matrix[m][n]/augmented_matrix[m][m]
-        for k in range(n-2,-1,-1):
-            x[k]=augmented_matrix[k][n]
-            for j in range(k+1,n):
-                x[k]=x[k]/augmented_matrix[k][k]
     
+    # #displaying solution 
+    # print("The following x vector matrix solves the above augmented matrix:")
+    # for answer in range(n):
+    #     print(f"x{answer} is {x[answer]}")
     
-    #displaying solution 
-    print("The following x vector matrix solves the above augmented matrix:")
-    for answer in range(n):
-        print(f"x{answer} is {x[answer]}")
+    return x
 
 
 
 
 
-variable_matrix = np.array([[1,1,3],[0,1,3],[-1,3,0]])
-constant_matrix = np.array([[1],[3],[5]])
+# variable_matrix = np.array([[1,1,3],[0,0,3],[-1,3,0]])
+# constant_matrix = np.array([[1],[3],[0]])
 
-gauss_elimination(variable_matrix, constant_matrix)
+# variable_matrix = np.array([[1,1,0],
+#                             [0,1,0],
+#                             [0,0,1]])
+# constant_matrix = np.array([[3],[2],[3]])
+# # 1 2 3
+
+# print(gauss_elimination(variable_matrix, constant_matrix))
